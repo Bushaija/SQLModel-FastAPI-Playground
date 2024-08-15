f51cbf5d1e6df04aa950b2ad85738b5bc1190006
bushaija <Bushaijarobert@gmail.com>
2024-08-15 14:30:10 +0200

    defined connection arguments

--- app/database.py
+++ app/database.py
@@ -1,11 +1,14 @@
-import sqlmodel as sql
+import sqlmodel as sql 
 
-# setup engine
-sqlite_file_name="database1.db"
+sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
-connect_args = {"check_same_thread":False}
-engine = sql.create_engine(sqlite_url, echo=True, connect_args=connect_args)
 
-# create db and tables
+connect_args = {"check_same_thread": False}
+engine = sql.create_engine(
+    sqlite_url, 
+    echo=True, 
+    connect_args=connect_args
+)
+
 def create_db_tables():
     sql.SQLModel.metadata.create_all(engine)
